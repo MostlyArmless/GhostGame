c4f86d97fb328124ea5aab230e2ce098328b8ece
MostlyArmless <mike.j.thiem@gmail.com>
2018-05-06 23:55:02 -0600

    Forfeit functionality seems to be working.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Readme for Ghost Game
+# Ghost - A Word Game
 
 ## Main Rules
-This is a 2+ player word game. The game starts by the first player picking a letter. Taking turns, each adds a letter to the current string. The first player to spell a real word becomes the Wordsmith, and are immediately killed. However, if you add a letter such that the string no longer _can become_ a word, you lose, but _only_ if the next player catches your bluff.
+This is a 2+ player word game. The game starts with the first player picking a letter. Taking turns, each player adds a letter to the current string. The first player to spell a real word becomes the Wordsmith, and is immediately killed. However, if you add a letter such that the string no longer _can become_ a word, you lose, but _only_ if the next player catches your bluff.
 
 On your turn, you can either add a letter to the end of the current string, or if you suspect the player before you couldn't think of a word and was just bluffing, you can challenge the last player's move. If you challenge the previous player, they must provide an example word that could be constructed from the current string (including the letter they just added). If they can't come up with an example, then they're out of the game. But if they _can_, you must be punished for your lack of faith: you're out of the game.
 
@@ -23,6 +23,7 @@ May the best man never spell anything!
 7. House Rules - Play continues after Wordsmith dies: Allow user to decide whether becoming a Wordsmith immediately ends the game, or whether the remaining players are allowed to continue using the string
 	NOTE: need to make a new temp dictionary by pruning the current string from the real dictionary
 
+8. Difficulty Modes: Implement multiple difficulty levels for AI players by defining different dictionary files containing simple VS rare words that they can draw from when playing, and making it so that the AI won't just randomly select a letter, but will always work towards building a possible word without actually completing it.
 
 #Developer Notes:
 Maybe I should add a PossibleWords property to GhostGame that contains a list of all possible words that could be constructed from the current string, and which updates every turn. This would be a good debugging tool, regardless. When there's lots of matches, it should display a sample 9 words: the 3 shortest words, 3 longest words, and 3 words from the middle of the list. Need to sort the list of candidate words by length and grab the first 3, last 3, and 3 from the middle with an even/odd length if statement.
--- ghost.py
+++ ghost.py
@@ -5,6 +5,8 @@ from string import ascii_letters as alphabet
 import random
 from randomdict import RandomDict
 from enum import Enum
+from pydispatch import dispatcher
+
 
 class PlayerStatus(Enum):
     """Enum for player status tracking"""
@@ -15,6 +17,7 @@ class PlayerStatus(Enum):
     @classmethod
     def has_value(self, value):
         return any(value == item.value for item in self)
+
 
 class ValidActions(Enum):
     """Enum to indicate the actions a player can take on their turn"""
@@ -27,48 +30,38 @@ class ValidActions(Enum):
         return any(value == item.value for item in self)
 
 
-class Player():
-    """AI player by default."""
+class Player:
+    """AI player"""
 
-    def __init__(self,name=None):
-        self.player_type = player_type
-        self.player_status = PlayerStatus.alive
-        self.__ai_names = ['Alouiciousness','Bobert','Cornelius','Danwise']
-        
-        if name is None:
-            self.name = random.choice(self.__ai_names)
-        else:
-            self.name = name
+    def __init__(self):
+        self.player_status = PlayerStatus.Alive
 
     def forfeit(self):
-        self.player_status = PlayerStatus.forfeit
-
+        self.player_status = PlayerStatus.Forfeit
+        dispatcher.send(signal=PlayerStatus.Forfeit, sender=self)
 
     def die(self):
         self.player_status = PlayerStatus.Killed
-        self.num_alive_players -= 1
-
+        dispatcher.send(signal=PlayerStatus.Killed, sender=self)
 
     def revive(self):
         self.player_status = PlayerStatus.Alive
-
+        dispatcher.send(signal=PlayerStatus.Alive, sender=self)
 
     def get_next_action(self):
         # The AI will always attempt to append a letter
-       return ValidActions.AppendLetter
-
+        return ValidActions.AppendLetter
 
     def get_letter(self):
         # Randomly select a letter from the alphabet
-        return random.choice(alphabet)
-
+        return random.choice(alphabet[0:26])
 
     def validate_letter(self, l):
         # Letter must be in [a-z]
         if len(l) != 1:
             return False
 
-        match = re.match(r"[a-zA-Z]",l)
+        match = re.match(r'[a-zA-Z]', l)
         if match is None:
             return False
         else:
@@ -77,21 +70,21 @@ class Player():
 
 class HumanPlayer(Player):
     """Overrides the necessary methods of Player class to allow for human interaction"""
+
     def __init__(self):
-        super()
+        super().__init__(self)
 
     def get_letter(self):
-        
+
         attempts = 0
         while attempts < 3:
             response = input('Enter a letter>>')
-            if self.validate_letter(response):
+            if super().validate_letter(self, response):
                 return response
             else:
                 attempts += 1
 
         raise ValueError('Too many attempts made, user is bad and dumb')
-
 
     def get_next_action(self):
         attempts = 0
@@ -105,26 +98,34 @@ class HumanPlayer(Player):
 
             if ValidActions.has_value(response):
                 is_valid = True
+                # Convert the int to an instance of the enum.
+                response = ValidActions(response)
             else:
                 attempts += 1
 
         if not is_valid:
             raise ValueError("User is bad and dumb")
 
+        return response
+
 
 class GhostGame():
-    """Tracks the game state"""
+    """Primary game object"""
 
     def help(self):
         with open(self.help_file) as file_obj:
             print(str(file_obj.read()))
 
-    def __init__(self,num_players=2,num_human_players=1,dict_file='dictionary1.txt'):
+    def __init__(self, num_players=2, num_human_players=1, dict_file='dictionary1.txt'):
+        # User Options
+        self.min_word_length = 3
+
+        # Primary Variables
         self.s = ''
         self.i_current_player = 0
-        self.players = self.init_players(num_players,num_human_players)
-        self.num_alive_players = len(players)
-        
+        self.players = self.init_players(num_players, num_human_players)
+        self.num_alive_players = len(self.players)
+
         # Resources
         self.dict_file = dict_file
         self.dict_url = "https://github.com/dwyl/english-words/raw/master/words_alpha.txt"
@@ -137,10 +138,8 @@ class GhostGame():
         self.word_list = self.dict_string.splitlines()
         self.word_set = self.build_word_set()
 
-
     def get_current_player(self):
         return self.players[self.i_current_player]
-
 
     def get_last_player(self):
         if self.i_current_player == 0:
@@ -148,17 +147,16 @@ class GhostGame():
         else:
             return self.players[self.i_current_player - 1]
 
-
-    def init_players(self,num_players,num_human_players):
+    def init_players(self, num_players, num_human_players):
         players = []
-        for i in range(0,num_players+1):
+        for i in range(0, num_players):
             if i < num_human_players:
                 players.append(HumanPlayer)
             else:
                 players.append(Player)
-
+            # Connect event handler
+            dispatcher.connect(signal=dispatcher.Any, receiver=self.handle_player_state_change, sender=players[i])
         return players
-    
 
     def next_player(self):
         if self.i_current_player == len(self.players) - 1:
@@ -166,35 +164,30 @@ class GhostGame():
         else:
             self.i_current_player += 1
 
-
     def download_dictionary_file(self):
         dict_file_path = Path(self.dict_file)
         word_set = set()
         if not dict_file_path.is_file():
             print('Downloading file...')
             # TODO: Add error handling for failed URL requests
-            urllib.request.urlretrieve(self.dict_url,dict_file_path)
-
+            urllib.request.urlretrieve(self.dict_url, dict_file_path)
 
     def get_dict_string(self):
         """Read the dictionary text file in as a single string for regexing"""
         with open(self.dict_file) as file_obj:
             return str(file_obj.read())
 
-
     def build_word_set(self):
         """Build a set of all words in the dictionary file"""
-        with open(self.dict_file,'r') as file_obj:
+        with open(self.dict_file, 'r') as file_obj:
             word_set = set()
             for line in file_obj:
                 word_set.add(line.rstrip())
             return word_set
 
-
     def check_word(self, test_word):
         """Check if a given test_word is an exact match for an existing dictionary word"""
         return test_word.lower() in self.word_set
-
 
     def check_possible_word(self, test_word):
         """Check if a test word is on it's way to becoming a possible word"""
@@ -213,11 +206,35 @@ class GhostGame():
 
     def print_string(self):
         print(f'Current string = "{self.s}"')
+        print('')
         # box_size = min(10,len(self.s))
         # horizontal_line = '-' * box_size
         # print(horizontal_line)
         # word_line = '|' + self.s + ' '*(box_size - len(self.s)) + '|'
-        # print(horizontal_line)       
+        # print(horizontal_line)
+
+    def handle_player_state_change(self, sender):
+        """Simple event handler"""
+        if sender.player_status == PlayerStatus.Alive:
+            self.num_alive_players += 1
+        elif sender.player_status == PlayerStatus.Killed:
+            self.num_alive_players -= 1
+        elif sender.player_status == PlayerStatus.Forfeit:
+            self.num_alive_players -= 1
+        else:
+            raise ValueError(f"Invalid PlayerStatus '{sender.player_status}'")
+
+    def game_over(self):
+        # TODO: Figure out why the Player class doesn't seem to have an attribute 'player_status' even though it's totally there. Inheritance problem maybe...?
+        # Find the alive player to give them credit
+        # for p in self.players:
+        #     if p.player_status == PlayerStatus.Alive:
+        #         alive_player = p
+        #         break
+
+        # print(f'GAME OVER. PLAYER {alive_player} WINS')
+        print('GAME OVER')
+        exit(0)
 
 
 def main():
@@ -225,6 +242,10 @@ def main():
 
     # Instantiate a GhostGame
     gg = GhostGame()
+
+    # Register the event handlers
+    # for player in gg.players:
+    #     dispatcher.connect(signal=dispatcher.Any, receiver=gg.handle_player_state_change, sender=player)
 
     # Game loop
     while True:
@@ -235,61 +256,48 @@ def main():
         gg.print_string()
 
         # Prompt the user for what action they want to take this turn
-        #TODO - figure out how to call this properly
+        # TODO - figure out how to call this properly
+        print(f'{this_player}\'s turn')
         this_action = this_player.get_next_action(this_player)
-        print(f'this_action = {this_action}')
 
         # Take the chosen action
         if this_action == ValidActions.AppendLetter:
             # Get the player's next letter
-            next_letter = this_player.get_letter()
+            next_letter = this_player.get_letter(this_player)
 
             # Append that letter to the string
             gg.s += next_letter
 
-        elif this_action == ValidActions.Forfeit:
+        elif this_action is ValidActions.Forfeit:
             # Player forfeits
-            this_player.forfeit()
-            print(f'{this_player.name} forfeits')
+            this_player.forfeit(this_player)
+            print(f'Player {gg.i_current_player} forfeits')
 
         elif this_action == ValidActions.Challenge:
             # Challenge the previous player
             if gg.check_possible_word(gg.s):
                 # It's possible for the previous player to construct a valid word.
                 # Ask the previous player to type the word they're thinking of
-                last_players_word = last_player.challenge()
+                last_players_word = last_player.challenge(last_player)
 
                 if gg.check_word(last_players_word):
                     # The word last_player provided IS in the dictionary, so they're safe.
                     # this_player made an erroneous challenge, so they die
-                    this_player.die()
+                    this_player.die(this_player)
                 else:
                     # last_player didn't provide a valid word, so they fail the challenge.
-                    last_player.die()
+                    last_player.die(last_player)
             else:
                 # The current string can't possibly be made into a word, so last_player loses
-                last_player.die()
+                last_player.die(last_player)
 
         # Check whether the end-game condition is satisfied
-        if self.num_alive_players == 0 or gg.check_word(gg.s):
+        if gg.num_alive_players < 2 or (len(gg.s) > gg.min_word_length and gg.check_word(gg.s)):
             gg.game_over()
-        
+
         # Move to the next player's turn
         gg.next_player()
-
-        if gg.check_word(gg.s + next_letter):
-            print(f'Player {this_player.name} loses! They made the word "{s_test}"')
-            gg.reset()
-        else:
-            # Append the letter
-            gg.s += next_letter
-
 
 
 if __name__ == '__main__':
     main()
-    # gg = GhostGame()
-    # gg.s = 'stuff'
-    # gg.print_string()
-
-
